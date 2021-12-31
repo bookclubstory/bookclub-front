97e8863d5dd4bada9da26717f066c4be39868b56
starrything@gmail.com <giskfmfqhrh14!>
2021-12-31 14:26:10 +0900

    update project directories

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import "react-app-polyfill/stable";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import "~/index.css";
+import "~/styles/index.css";
 import "~/styles/carousel.css";
 import App from "~/App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "~/modules";
@@ -48,4 +48,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
