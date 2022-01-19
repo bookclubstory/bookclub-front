ddb7cd685b59041ead43da7a379f84245e9fb3d8
390815 <sora.son@iljin.co.kr>
2022-01-19 16:40:25 +0900

    2022.1.19 * Footer.tsx 디자인 추가

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -2,33 +2,35 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { ReducerType, resetStore } from "@modules/index";
-import { Search, Icon } from 'semantic-ui-react';
+import {Container, Search, Icon, } from 'semantic-ui-react';
 
 const Gnb = (props: any) => {
   const dispatch = useDispatch();
   const token = useSelector((state: ReducerType) => state.session.loginInfo.token);
 
   return (
-      <nav className="ui large inverted pointing fixed menu">
-        <Link
-          to="/"
-          className='header item'
-          onClick={() => dispatch(resetStore())}>
-          <Icon name='book'/>
-          우동북
-        </Link>
-        <div className='item'>
-          <Search></Search>
-        </div>
+        <nav className="ui large inverted top fixed menu">
+            <Container fluid>
+                <Link
+                  to="/"
+                  className="header item"
+                  onClick={() => dispatch(resetStore())}>
+                  <Icon name='book'/>
+                  우동북
+                </Link>
+                <div className='item'>
+                  <Search></Search>
+                </div>
 
-        <div className='right item'>
-          <Link to="/overview" className="item">Overview</Link>
-          <Link to="/bookclub/list" className="item">북클럽</Link>
-          <Link to="/bookpost/list" className="item">독서로그</Link>
-          <Link to="/login" className="ui button" >Log in</Link>
-          <Link to="/signup" className="ui button" style={{ marginLeft: '0.5em' }}>Sign Up</Link>
-        </div>
-      </nav>
+                <div className='right item'>
+                  <Link to="/overview" className="item">Overview</Link>
+                  <Link to="/bookclub/list" className="item">북클럽</Link>
+                  <Link to="/bookpost/list" className="item">독서로그</Link>
+                  <Link to="/login" className="ui button" >Log in</Link>
+                  <Link to="/signup" className="ui button" style={{ marginLeft: '0.5em' }}>Sign Up</Link>
+                </div>
+            </Container>
+        </nav>
   );
 };
 
