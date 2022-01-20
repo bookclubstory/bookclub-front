f782c6ac77ab4df9f47b80e2503d72b384a79d02
390815 <sora.son@iljin.co.kr>
2022-01-20 17:22:58 +0900

    2022.1.20 * material ui로 변경 header/footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,38 @@
 import React from "react";
-import {Container, Grid, Header, List, Segment} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+import {Box, Container, Typography} from "@mui/material";
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link to="/">
+                Jonghyun, Inc.
+            </Link>{' '}
+            {new Date().getFullYear()} ·
+            <Link to="#">Privacy</Link> ·{" "}<Link to="#">Terms</Link>{'.'}
+        </Typography>
+    );
+}
 
 const Footer = (props: any) => {
     return (
-        <Segment inverted vertical style ={{margin: '3em 0em 0em',padding: '3em 3em'}}>
-            <Container fluid>
-                <Grid divided inverted stackable textAlign='left'>
-                    <Grid.Row>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='About' />
-                            <List link inverted>
-                                <List.Item as='a'>Sitemap</List.Item>
-                                <List.Item as='a'>Contact Us</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={3}>
-                            <Header inverted as='h4' content='Services' />
-                            <List link inverted>
-                                <List.Item as='a'>DNA FAQ</List.Item>
-                                <List.Item as='a'>How To Access</List.Item>
-                            </List>
-                        </Grid.Column>
-                        <Grid.Column width={7}>
-                            <Header as='h4' inverted>
-                                Footer Header
-                            </Header>
-                            <p>
-                                Extra space for a call to action inside the footer that could help re-engage users.
-                            </p>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+        <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
+            <Container maxWidth="lg">
+                <Typography variant="h6" align="center" gutterBottom>
+                  Footer
+                </Typography>
+                <Typography
+                    variant="subtitle1"
+                    align="center"
+                    color="text.secondary"
+                    component="p"
+                >
+                </Typography>
+                <Copyright />
             </Container>
-        </Segment>
+        </Box>
     );
 };
 
