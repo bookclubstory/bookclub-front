1754db283275d28a0b1d45c25a5dd961093d5d8d
starrything@gmail.com <giskfmfqhrh14!>
2022-01-28 13:42:32 +0900

    로그인 처리 1차 (TODO: 세션 관리)

--- src/components/Gnb.tsx
+++ src/components/Gnb.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext, useState, useEffect, MouseEvent } from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { ReducerType, resetStore } from "@modules/index";
+import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
+import { ReducerType, resetStore, initStore } from "@modules/index";
+import { SessionContext } from "@utils/session";
 import {
   alpha,
   AppBar,
@@ -16,6 +17,10 @@ import {
 } from "@mui/material";
 import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 import SearchIcon from "@mui/icons-material/Search";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import * as session from "@utils/session";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -57,11 +62,104 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+const SigninBlock = (props: any) => {
+  const dispatch = useDispatch();
+  const session = useContext(SessionContext);
+  // Description: 로그인 후 개인 메뉴 기능 (아이콘 드랍다운 메뉴)
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const signout = () => {
+    // 1. clear redux store
+    dispatch(initStore())
+    // 2. clear browser Local storage
+    global.localStorage.clear();
+    // 3. clear Cookies
+    //session.removeCookie("session");
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    handleClose();
+  }
+
+  if (props.signinYn) {
+    return (
+      <div>
+        <IconButton
+          sx={{ my: 2, mx: 1, color: "white" }}
+          aria-label="my profile"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <AccountCircleIcon />
+        </IconButton>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>My account</MenuItem>
+          <MenuItem onClick={() => signout()}>Logout</MenuItem>
+        </Menu>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Button
+          color="inherit"
+          variant="outlined"
+          size="small"
+          sx={{ my: 2, mx: 1 }}
+          component={Link}
+          to={"/login"}
+        >
+          Log in
+        </Button>
+        <Button
+          color="inherit"
+          variant="outlined"
+          size="small"
+          sx={{ my: 2, mx: 1 }}
+          component={Link}
+          to={"/signup"}
+        >
+          Sign Up
+        </Button>
+      </div>
+    );    
+  }
+};
+
 const Gnb = (props: any) => {
   const dispatch = useDispatch();
-  const token = useSelector(
-    (state: ReducerType) => state.session.loginInfo.token
-  );
+  const session = useContext(SessionContext);
+  const [signinYn, setSigninYn] = useState<boolean>(false);
+  let storedSession = useSelector((state: RootStateOrAny) => state.session.loginInfo);
+
+  useEffect(() => {
+    console.log("Changed session...!")
+    console.log(storedSession);
+    if (storedSession.loginId) {
+      console.log("useEffect: true");
+      setSigninYn(true);
+    } else {
+      console.log("useEffect: false");
+      setSigninYn(false);      
+    }
+  }, [storedSession]);
 
   return (
     <React.Fragment>
@@ -123,26 +221,7 @@ const Gnb = (props: any) => {
             >
               독서로그
             </Button>
-            <Button
-              color="inherit"
-              variant="outlined"
-              size="small"
-              sx={{ my: 2, mx:1 }}
-              component={Link}
-              to={"/login"}
-            >
-              Log in
-            </Button>
-            <Button
-              color="inherit"
-              variant="outlined"
-              size="small"
-              sx={{ my: 2, mx:1 }}
-              component={Link}
-              to={"/signup"}
-            >
-              Sign Up
-            </Button>
+            <SigninBlock signinYn={signinYn} />
           </Box>
         </Toolbar>
       </AppBar>
