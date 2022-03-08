5161f228ecf122e066cee2cef38f31adb8ae30e4
ssr03 <sora.son@iljin.co.kr>
2022-03-08 14:45:09 +0900

    2022.3.8 북클럽 페이지 추가 * 북클럽 페이지 UI(ClubDetail.tsx추가) * ClubHeader.tsx에 북클럽 기본 데이터 조회 되도록 수정

--- src/pages/bookclub/Bookclub.tsx
+++ src/pages/bookclub/Bookclub.tsx
@@ -7,39 +7,66 @@ import ClubMain from "@components/Bookclub/ClubMain";
 import axiosConfig from "@utils/axiosConfig";
 import * as actionOfClubAuth from "@modules/code/actionOfClubAuth";
 import {useDispatch} from "react-redux";
+import ClubDetail from "@components/Bookclub/ClubDetail";
+import {useParams} from "react-router";
 
 const header = {
     clubId: "1",
-    title: '공덕역 방구석 철학자들',
-    image: 'https://source.unsplash.com/random',
-    subtitle: "혼자선 읽기 힘든 철학서적들 독서 품앗이로 함께 읽어요",
-    memberCnt: 4,
-    memberTotCnt: 10,
-    username: "Starrything",
-    description: "EXIT를 꿈꾸는 개발자"
+    clubNm: '공덕역 방구석 철학자들',
+    clubLoc: "서울",
+    memberCnt: 0,
+    totMemberCnt:0,
+    privateYn:false,
+    clubIntro: '혼자선 읽기 힘든 철학서적들 독서 품앗이로 함께 읽어요',
+    thumbnail: 'https://source.unsplash.com/random',
+    owner:{
+        username: "Starrything",
+        description: "꿈꾸는 개발자"
+    }
 }
 
 interface Bookclub {
-    main:string
+    header: {
+        clubId: string,
+        clubNm: string,
+        clubLoc: string,
+        memberCnt: number,
+        totMemberCnt:number,
+        privateYn:boolean,
+        clubIntro: string,
+        thumbnail: string,
+        owner:{
+            username: string,
+            description:string,
+        }
+    },
+    main:string,
+    clubId: string,
 }
 
 const Bookclub = (props: any) => {
     const dispatch = useDispatch();
+    const {clubId} = useParams<Bookclub["clubId"]>();
 
     const [main, setMain] = useState<Bookclub["main"]>("")
     const [loading, setLoading] = useState(false);
+    const [bookclub, setBookclub] = useState<Bookclub["header"]>(header);
 
     useEffect(() => {
         getAuthCodeList();
+        getBookclub(clubId);
     }, []);
 
-    const showMain = (clubId: string) => {
+    const showMain = (bookclub: Bookclub["header"]) => {
         switch(main){
+            case "main":
+                return <ClubDetail clubId = {bookclub.clubId} clubIntro={bookclub.clubIntro} />
+                break;
             case "member":
-                return <ClubMemberList clubId = {clubId} />
+                return <ClubMemberList clubId = {bookclub.clubId} />
                 break;
             default:
-                return <ClubMemberList clubId = {clubId}/>
+                return <ClubDetail clubId = {bookclub.clubId} clubIntro={bookclub.clubIntro}/>
         }
     }
 
@@ -69,12 +96,25 @@ const Bookclub = (props: any) => {
         });
     };
 
+    const getBookclub = (clubId: any) => {
+        axiosConfig.get(`/api/v1/bookclub/${clubId}`)
+            .then(function (response: any) {
+                setBookclub(response.data);
+            })
+            .catch(function (error) {
+                // error
+            })
+            .then(function () {
+                // finally
+            });
+    }
+
     return (
         <Container component="main" sx={{mt:1.5}} >
-            <ClubHeader header={header} setMain={setMain} />
+            <ClubHeader header={bookclub} setMain={setMain} />
 
             {loading&&
-                <ClubMain main={showMain(header.clubId)}/>}
+                <ClubMain main={showMain(bookclub)}/>}
         </Container>
     );
 }
